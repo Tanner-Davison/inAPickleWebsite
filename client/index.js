1c337742232edf757fe991474f1a07fe552fffb4
tannerdavison <tboydavison@gmail.com>
2023-06-03 10:37:02 -0600

    saved some color preferences

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ const form = document.getElementById('formId');
 const displayCourts = document.getElementById('sectionContainer');
 const courtObjects = []
 const baseURL = 'http://localhost:4500/api'
-let isNewCourtAdded = false;
+
 
 // create class of Court to generate court objects
 
@@ -22,49 +22,50 @@ function generateCourtInfo(court) {
         // Court with the same ID already exists, do not add it again
         return;
       }
-   
     let courtInfo = document.createElement('div');
-   
-    // for TILT.JS
-    courtInfo.classList.add('courtCard');
+    courtInfo.classList.add('courtCard');         
+
+    courtInfo.setAttribute('data-tilt-glare', 'true');      // for TILT.JS
+    courtInfo.setAttribute('data-tilt-perspective', '2000');// for TILT.JS
+    courtInfo.setAttribute('data-tilt-scale', '1');         // for TILT.JS
+    courtInfo.setAttribute('data-tilt-speed', '400');       // for TILT.JS
+    courtInfo.setAttribute('data-tilt-max', '30');          // for TILT.JS
     
-    courtInfo.setAttribute('data-tilt-glare', 'true');
-    courtInfo.setAttribute('data-tilt-perspective', '2000');
-    courtInfo.setAttribute('data-tilt-scale', '1');
-    courtInfo.setAttribute('data-tilt-speed', '400');
-    courtInfo.setAttribute('data-tilt-max', '30');
-    // court info to display
-    
-    courtInfo.innerHTML = `
+    courtInfo.innerHTML = `                                 
         <h1>${court.name}'s Court</h1>
         <div id='dimensions'>
         <div id='courtWithWidth'>
-        <div style='display:flex, flex-direction:column;font-size:18px;'>
-            <u>Width</u>
-            <p style ='color:red'>${court.width}ft</p>
-            </div>
+        <div style='display:flex, flex-direction:column;font-size:18px; transform: translateZ(800px);'>
+        <u>Width</u>
+        <p style ='color:red'>${court.width}ft</p>
+        </div>
         <img alt="Pickleball Court" id="courtIMG" src="/photos/Screenshot 2023-05-29 at 9.27.31 PM.png"width='200' height='200'>
-            </div>
-        <div style='display:flex, flex-direction:column;font-size:18px;'>
-            <u>Length</u>
-            <p style ='color:red'>${court.length} ft</p>
         </div>
-        <br>
         <div style='display:flex, flex-direction:column;font-size:18px;'>
-            <u>Area:</u>
-            <p style ='color:red'>${court.area} ft</p>
+        <u>Length</u>
+        <p style ='color:red'>${court.length} ft</p>
+        </div><br>
+        <div style='display:flex, flex-direction:column;font-size:18px;'>
+        <u>Area:</u>
+        <p style ='color:red'>${court.area} ft</p>
         </div>
-        <p>Court # ${court.id}</p>
-    </div>
+        <p>Court # ${court.id-1}</p>
+    
+
     <div class='cardBtnContainer'>
         <button class='btnCard' id='notesBtn'>Notes</button>
         <button class='btnCard' id='infoBtn'>Court Info</button>
         <button class='btnCard' id='customerInfo'>Customer Details</button>
         <button class='btnCard' id='deleteBTN' onClick='deleteCourt(${court.id})'>Delete Court</button>
-    </div>`
+    </div>
+    </div>
+    
+    `
+    
     console.log(court.id)
      if(court.id>1){                
     displayCourts.appendChild(courtInfo);
+    displayCourts.style='background-color: black'
      }else{
         return;
      }
@@ -74,6 +75,21 @@ function generateCourtInfo(court) {
     scale: 1.1,
     });
 
+    const notesBtn = courtInfo.querySelector('#notesBtn');
+    const backBtn = courtInfo.querySelector('#backBtn');
+    const courtFront = courtInfo.querySelector('.courtFront');
+    const courtBack = courtInfo.querySelector('.courtBack');
+  
+    notesBtn.addEventListener('click', () => {
+      courtFront.classList.add('flipped');
+      courtBack.classList.add('flipped');
+    });
+  
+    backBtn.addEventListener('click', () => {
+      courtFront.classList.remove('flipped');
+      courtBack.classList.remove('flipped');
+    });
+  
     return courtInfo;
     }
 
@@ -118,18 +134,7 @@ function onClick(event){
       displayCourts.innerHTML = '';
       res.data.forEach((courtObj) => {
         generateCourtInfo(courtObj);
-      });
-
-      if (!isNewCourtAdded) {
-        // Add the "Add New Court" heading to the form
-        const addNewCourt = document.createElement('div');
-        addNewCourt.innerHTML = `
-          <h1>Add New Court</h1>
-        `;
-        addNewCourt.classList.add('addNewCourt');
-        form.appendChild(addNewCourt);
-        isNewCourtAdded = true;
-      }
+      })
     })
     .catch((error) => {
       console.log(error);
